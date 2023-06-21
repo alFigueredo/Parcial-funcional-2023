48620fa4ab876befa97a586c2418ef330de51c5d
Alan Lionel Figueredo Aguiar <lionelfigueredo@gmail.com>
2023-06-21 20:19:48 -0300

    Solución v1

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,82 @@
 module Library where
 import PdePreludat
 
-doble :: Number -> Number
-doble numero = numero + numero
+data Animal = Animal {
+  nombreAnimal :: String,
+  tipoAnimal :: String,
+  peso :: Number,
+  edad :: Number,
+  estaEnfermo :: Bool,
+  visitasMedicas :: [VisitaMedica]
+} deriving (Show, Eq)
+
+data VisitaMedica = VisitaMedica {
+  diasRecuperacion :: Number,
+  costo :: Number
+} deriving (Show, Eq)
+
+-- Punto 1
+
+laPasoMal :: Animal -> Bool
+laPasoMal = any ((>30) . diasRecuperacion) . visitasMedicas
+
+nombreFalopa :: Animal -> Bool
+nombreFalopa animal = (=="i") . drop (subtract 1 . length . nombreAnimal $ animal) . nombreAnimal $ animal
+
+-- Punto 2
+
+type Actividad = Animal -> Animal
+
+modificarPeso :: Number -> Animal -> Animal
+modificarPeso kilos animal = animal {
+  peso = (+kilos) . peso $ animal
+}
+
+engorde :: Number -> Actividad
+engorde kilos = modificarPeso (min 5 . flip (/) 2 $ kilos)
+
+revisacion :: VisitaMedica -> Actividad
+revisacion visitaMedica animal
+  | estaEnfermo animal = engorde 2 animal {
+    visitasMedicas = (++[visitaMedica]) . visitasMedicas $ animal
+  }
+  | otherwise = animal
+
+festejoCumple :: Actividad
+festejoCumple animal = modificarPeso (-1) animal {
+  edad = (+1) . edad $ animal
+}
+
+chequeoDePeso :: Number -> Actividad
+chequeoDePeso pesoMinimo animal = animal {
+  estaEnfermo = not ((>pesoMinimo) . peso $ animal) || estaEnfermo animal
+}
+
+-- Punto 3
+
+type Proceso = [Actividad]
+
+ejemploProceso :: Proceso
+ejemploProceso = [engorde 4, revisacion (VisitaMedica 20 400), festejoCumple, chequeoDePeso 500]
+
+aplicarProceso :: Proceso -> Actividad
+aplicarProceso proceso animal = foldl (flip ($)) animal proceso
+
+-- Ejemplo: aplicarProceso ejemploProceso dorothy
+
+-- Punto 4
+
+mejoraONoMejora :: Proceso -> Animal -> Bool
+mejoraONoMejora [] _ = True
+mejoraONoMejora (actividad:actividades) animal = mejoraSustentable animal actividad && (mejoraONoMejora actividades . actividad) animal
+
+mejoraSustentable :: Animal -> Actividad -> Bool
+mejoraSustentable animal actividad = rangoMejora . subtract (peso animal) . peso . actividad $ animal
+  where rangoMejora diferenciaPeso = (>=0) diferenciaPeso && (<=3) diferenciaPeso
+
+-- Punto 5
+
+giveMeThree :: [Animal] -> [Animal]
+giveMeThree = take 3 . filter nombreFalopa
+
+-- Según la evaluación diferida, la función toma solo lo que necesita. En este caso, si la cantidad de animales que se le pasa a la función giveMeThree es infinita, s por lo tanto, la función convergería siempre y cuando logre encontrar tres animales con nombre falopa.
--- src/Spec.hs
+++ src/Spec.hs
@@ -3,9 +3,95 @@ import PdePreludat
 import Library
 import Test.Hspec
 
+dorothy :: Animal
+dorothy = Animal {
+  nombreAnimal = "Dorothy",
+  tipoAnimal = "Vaca",
+  peso = 690,
+  edad = 20,
+  estaEnfermo = False,
+  visitasMedicas = [VisitaMedica 20 500, VisitaMedica 40 800]
+}
+
+pachi :: Animal
+pachi = Animal {
+  nombreAnimal = "Pachi",
+  tipoAnimal = "Vaca",
+  peso = 560,
+  edad = 5,
+  estaEnfermo = True,
+  visitasMedicas = []
+}
+
+gachi :: Animal
+gachi = Animal {
+  nombreAnimal = "Gachi",
+  tipoAnimal = "Vaca",
+  peso = 560,
+  edad = 5,
+  estaEnfermo = False,
+  visitasMedicas = []
+}
+
+proceso1 :: Proceso
+proceso1 = [engorde 4, revisacion (VisitaMedica 20 400), chequeoDePeso 500]
+
+proceso2 :: Proceso
+proceso2 = [revisacion (VisitaMedica 20 400), engorde 8, chequeoDePeso 500]
+
 correrTests :: IO ()
 correrTests = hspec $ do
-  describe "Test de ejemplo" $ do
-    it "El pdepreludat se instaló correctamente" $ do
-      doble 1 `shouldBe` 2
 
+  -- Punto 1
+
+  describe "Tests punto 1" $ do
+    it "Función laPasoMal" $ do
+      dorothy `shouldSatisfy` laPasoMal
+      pachi `shouldNotSatisfy` laPasoMal
+    it "Función nombreFalopa" $ do
+      dorothy `shouldNotSatisfy` nombreFalopa
+      pachi `shouldSatisfy` nombreFalopa
+  
+  -- Punto 2
+
+  describe "Tests punto 2" $ do
+    it "Actividad engorde" $ do
+      (peso . engorde 12) dorothy `shouldBe` 695
+      (peso . engorde 4) dorothy `shouldBe` 692
+    it "Actividad revisacion si el animal no está enfermo" $ do
+      (peso . revisacion (VisitaMedica 20 400)) dorothy `shouldBe` 690
+      (visitasMedicas . revisacion (VisitaMedica 20 400)) dorothy `shouldMatchList` [VisitaMedica 20 500, VisitaMedica 40 800]
+    it "Actividad revisacion si el animal está enfermo" $ do
+      (peso . revisacion (VisitaMedica 20 400)) pachi `shouldBe` 561
+      (visitasMedicas . revisacion (VisitaMedica 20 400)) pachi `shouldMatchList` [VisitaMedica 20 400]
+    it "Actividad festejoCumple" $ do
+      (edad . festejoCumple) dorothy `shouldBe` 21
+      (peso . festejoCumple) dorothy `shouldBe` 689
+    it "Actividad chequeoDePeso" $ do
+      (estaEnfermo . chequeoDePeso 500) dorothy `shouldBe` False
+      (estaEnfermo . chequeoDePeso 700) dorothy `shouldBe` True
+      (estaEnfermo . chequeoDePeso 500) pachi `shouldBe` True
+      (estaEnfermo . chequeoDePeso 700) pachi `shouldBe` True
+
+  -- Punto 3
+
+  describe "Tests punto 3" $ do
+    it "Función aplicarProceso" $ do
+      (peso . aplicarProceso ejemploProceso) dorothy `shouldBe` 691
+      (visitasMedicas . aplicarProceso ejemploProceso) dorothy `shouldMatchList` [VisitaMedica 20 500, VisitaMedica 40 800]
+      (edad . aplicarProceso ejemploProceso) dorothy `shouldBe` 21
+      (estaEnfermo . aplicarProceso ejemploProceso) dorothy `shouldBe` False
+
+  -- Punto 4
+
+  describe "Tests punto 4" $ do
+    it "Función mejoraONoMejora" $ do
+      dorothy `shouldNotSatisfy` mejoraONoMejora ejemploProceso
+      dorothy `shouldSatisfy` mejoraONoMejora proceso1
+      dorothy `shouldNotSatisfy` mejoraONoMejora proceso2
+  
+  -- Punto 5
+
+  describe "Tests punto 5" $ do
+    it "Función giveMeThree" $ do
+      giveMeThree [dorothy, pachi, dorothy, gachi, dorothy, pachi, gachi, dorothy] `shouldMatchList` [pachi, gachi, pachi]
